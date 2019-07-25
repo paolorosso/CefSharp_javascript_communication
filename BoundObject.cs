e8740444a7dc2be71c332f4127c52982d45fc750
Paolo Rosso <paolo@i2k.it>
2019-07-25 08:49:56 +0200

    init

---
+++ BoundObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CefSharp_javascript_communication
+{
+    class BoundObject
+    {
+        Frm_webrowser frmMain = null;
+
+        public BoundObject(Frm_webrowser frm)
+        {
+            frmMain = frm;
+        }
+
+        public void TestWinForm(string text)
+        {
+            frmMain.testWinForm(text);
+        }
+
+
+    }
+}
---
+++ Frm_webrowser.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace CefSharp_javascript_communication
+{
+    public partial class Frm_webrowser : Form
+    {
+        public Frm_webrowser()
+        {
+            InitializeComponent();
+
+            // Start the browser after initialize global component
+            InitializeChromium();
+        }
+
+        public ChromiumWebBrowser chromeBrowser;
+        public string Url = System.IO.Path.GetFullPath("../../../") + @"\HTML\test.html";
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser(Url);
+
+            // Eventi browser
+            chromeBrowser.LoadingStateChanged += ChromeBrowser_LoadingStateChanged;
+
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+
+            chromeBrowser.JavascriptObjectRepository.Register("boundAsync", new BoundObject(this), false);
+
+        }
+
+
+
+        private void ChromeBrowser_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+
+            // Wait for the Page to finish loading
+            if (e.IsLoading == false)
+            {
+                // X DEBUG
+                chromeBrowser.ShowDevTools();
+
+            }
+        }
+
+        private void Frm_webrowser_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+
+
+
+        internal void testWinForm(string text)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                MessageBox.Show(text);
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str;
+            str = "jsFunction('from winForm')";
+
+            chromeBrowser.ExecuteScriptAsync(str);
+        }
+
+    }
+
+}
